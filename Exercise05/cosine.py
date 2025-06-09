cbaf3564b3f60c5d5539c4a1055865aa730ed604
hasabir <hasabir@student.1337.ma>
2025-06-09 17:04:29 +0100

    retoutching of ex02, ex03, ex04, ex05, ex06

--- Exercise05/cosine.py
+++ Exercise05/cosine.py
@@ -18,36 +18,3 @@ def angle_cos(u: Vector[K], v: Vector[K]) -> f32:
     if u_norm == 0 or v_norm == 0:
         raise ZeroDivisionError("Zero Division Error")
     return u.dot(v) / (u_norm * v_norm)
-
-
-def main():
-    try:
-        u = Vector([1., 0.])
-        v = Vector([1., 0.])
-        print(angle_cos(u, v))
-
-        u = Vector([1., 0.])
-        v = Vector([0., 1.])
-        print(angle_cos(u, v))
-
-        u = Vector([-1., 1.])
-        v = Vector([1., 0.])
-        print(angle_cos(u, v))
-
-        u = Vector([2., 1.])
-        v = Vector([4., 2.])
-        print(angle_cos(u, v))
-
-        u = Vector([1., 2., 3.])
-        v = Vector([4., 5., 6.])
-        print(angle_cos(u, v))
-
-        # print(angle_cos(u, None))
-        # print(angle_cos(u, Vector([0, 0])))
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-
-if __name__ == "__main__":
-    main()
---
+++ Exercise05/main.py
@@ -0,0 +1,36 @@
+from cosine import angle_cos
+from vector import Vector
+from matrix import Matrix
+
+
+def main():
+    try:
+        u = Vector([1., 0.])
+        v = Vector([1., 0.])
+        print(angle_cos(u, v))
+
+        u = Vector([1., 0.])
+        v = Vector([0., 1.])
+        print(angle_cos(u, v))
+
+        u = Vector([-1., 1.])
+        v = Vector([1., 0.])
+        print(angle_cos(u, v))
+
+        u = Vector([2., 1.])
+        v = Vector([4., 2.])
+        print(angle_cos(u, v))
+
+        u = Vector([1., 2., 3.])
+        v = Vector([4., 5., 6.])
+        print(angle_cos(u, v))
+
+        # print(angle_cos(u, None))
+        # print(angle_cos(u, Vector([0, 0])))
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+if __name__ == "__main__":
+    main()
