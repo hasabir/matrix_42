fbcc4f3777247f9edf1840febfd47d0bfaa166cf
hasabir <hasabir@student.1337.ma>
2025-06-10 13:17:56 +0100

    retoutching of ex07

--- Exercise07/main.py
+++ Exercise07/main.py
@@ -7,67 +7,67 @@ from matrix import Matrix
 
 
 def main():
-    # try:
-    u = Matrix(
-        [[1., 0.],
-        [0., 1.]]
-    )
-    v = Vector([4., 2.])
-    print(u.mul_vec(v))
-    print("******************")
-    
-    
-    u = Matrix(
-        [[2., 0.],
-        [0., 2.]]
-    )
-    v = Vector([4., 2.])
-    print(u.mul_vec(v))
-    print("******************")
-    
-    u = Matrix(
-        [[2., -2.],
-        [-2., 2.]]
-    )
-    v = Vector([4., 2.])
-    print(u.mul_vec(v))
-    print("***************** matrix multiplication ***************")
-    
-    u = Matrix(
-        [[1., 0.],
-        [0., 1.]]
-    )
-    v = Matrix(
-        [[1., 0.],
-        [0., 1.]]
-    )
-    print(u.mul_mat(v))
-    print("******************")
-    
-    
-    u = Matrix(
-        [[1., 0.],
-        [0., 1.]]
-    )
-    v = Matrix(
-        [[2., 1.],
-        [4., 2.]]
-    )
-    print(u.mul_mat(v))
-    print("******************")
-    u = Matrix(
-        [[3., -5.],
-        [4., 2.]]
-    )
-    v = Matrix(
-        [[2., 1.],
-        [4., 2.]]
-    )
-    print(u.mul_mat(v))
-    print("******************")
-    
-    # except Exception as e:
-    #     print(f"Error: {e}")
+    try:
+        u = Matrix(
+            [[1., 0.],
+            [0., 1.]]
+        )
+        v = Vector([4., 2.])
+        print(u.mul_vec(v))
+        print("******************")
+        
+        
+        u = Matrix(
+            [[2., 0.],
+            [0., 2.]]
+        )
+        v = Vector([4., 2.])
+        print(u.mul_vec(v))
+        print("******************")
+        
+        u = Matrix(
+            [[2., -2.],
+            [-2., 2.]]
+        )
+        v = Vector([4., 2.])
+        print(u.mul_vec(v))
+        print("***************** matrix multiplication ***************")
+        
+        u = Matrix(
+            [[1., 0.],
+            [0., 1.]]
+        )
+        v = Matrix(
+            [[1., 0.],
+            [0., 1.]]
+        )
+        print(u.mul_mat(v))
+        print("******************")
+        
+        
+        u = Matrix(
+            [[1., 0.],
+            [0., 1.]]
+        )
+        v = Matrix(
+            [[2., 1.],
+            [4., 2.]]
+        )
+        print(u.mul_mat(v))
+        print("******************")
+        u = Matrix(
+            [[3., -5.],
+            [6., 8.]]
+        )
+        v = Matrix(
+            [[2., 1.],
+            [4., 2.]]
+        )
+        print(u.mul_mat(v))
+        print("******************")
+        
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
--- Exercise08/main.py
+++ Exercise08/main.py
@@ -3,23 +3,39 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), '../')))
 from vector import Vector
 from matrix import Matrix
-import traceback
+
 
 def main():
     try:
-        m = Matrix([[1, 2], [3, 4]])
+        m = Matrix(
+            [[1, 2],
+            [3, 4]])
         print(m.trace())
-        m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        print(m.trace())
-        m = Matrix([[1]])
-        print(m.trace())
-        m = Matrix([[1, 2], [3, 4], [5, 6]])
-        print(m.trace())
-        
         print("********************")
         
-        u = Matrix([[1, 0], [0, 1]])
+        
+        m = Matrix([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]])
+        print(m.trace())
+        print("********************")
+        
+        m = Matrix([[1]])
+        print(m.trace())
+        print("********************")
+        
+        m = Matrix([[1, 2],
+                    [3, 4],
+                    [5, 6]])
+        print(m.trace())
+        print("********************")
+        
+        
+        u = Matrix([[1, 0],
+                    [0, 1]])
         print(u.trace())
+        print("********************")
         
         u = Matrix([
             [2., -5., 0.],
@@ -29,7 +45,6 @@ def main():
         print(u.trace())
     except Exception as e:
         print(f"Error: {e}")
-        # traceback.print_exc()
         
 
 if __name__ == "__main__":
