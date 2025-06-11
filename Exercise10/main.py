4c42fbf4e1938ad176d88b92326c51adcb58b182
Hajar Sabir <96284181+hasabir@users.noreply.github.com>
2025-06-11 16:26:26 +0000

    ex 10

--- Exercise10/main.py
+++ Exercise10/main.py
@@ -7,117 +7,33 @@ from matrix import Matrix
 
 
 def main():
-    m = [[0., 0.,0., 0.],
-         [0., 0., 0., 0.],
-         [2., 9., -3., 8.],
-         [6., 13., -1., 14]]
-
-    m1 = [[0., 0.,0., 0.],
-         [0., 5., 4., 0.],
-         [0., 0., 3., 0.],
-         [0., 0., 0., 2.]]
+    u = Matrix([[1., 0., 0.],
+                [0., 1., 0.],
+                [0., 0., 1.],])
+    print(u.row_echelon())
+    print("*******************************")
 
 
-    m2 = [[6, -2, 3],
-          [4., 0., 7.],
-          [5., 0., 7.],
-          [0., -1., 3]]
+    u = Matrix([[1., 2.],
+                [3., 4.],])
+    print(u.row_echelon())
+    print("*******************************")
     
-    m3 = Matrix([[1, 2, 3, 4],
-          [0, 0, 0, 0],
-          [0, 0, 5, 6],
-          [2, 5, 4, 5]])
-    
-    u = Matrix([[8., 5., -2., 4., 28.],[4., 2.5, 20., 4., -4.],[8., 7., 1., 4., 17.],])
-    # u = Matrix([[8., 5., -2., 4., 28.],[4., 2.5, 20., 4., -4.],[8., 5., 1., 4., 17.],[8., 7., 1., 4., 17.],])
-    
-    m = Matrix(m)
-    m1 = Matrix(m1)
-    m2 = Matrix(m2)
-     
-    # print(f"matrix before row echelon form:\n{m3}")
-    print("*************************\n matrix after row echelon form:\n")
-#     print(m.row_echelon())
-    print(m.row_echelon())
-#     print(m2.row_echelon())
 
+    u = Matrix([[1., 2.],
+                [2., 4.],])
+    print(u.row_echelon())
+    print("*******************************")
+    
+
+    
+    u = Matrix([[8., 5., -2., 4., 28.],
+                [4., 2.5, 20., 4., -4.],
+                [8., 5., 1., 4., 17.],])
+    print(u.row_echelon())
+    print("*******************************")
+    
 
 if __name__ == "__main__":
     main()
 
-
-
-# # import numpy as np
-# # # Function to check if matrix is in REF
-
-# # def is_row_echelon_form(matrix):
-# #     if not matrix.any():
-# #         return False
-
-#     rows = matrix.shape[0]
-#     cols = matrix.shape[1]
-#     prev_leading_col = -1
-
-#     for row in range(rows):
-#         leading_col_found = False
-#         for col in range(cols):
-#             if matrix[row, col] != 0:
-#                 if col <= prev_leading_col:
-#                     return False
-#                 prev_leading_col = col
-#                 leading_col_found = True
-#                 break
-#         if not leading_col_found and any(matrix[row, col] != 0 for col in range(cols)):
-#             return False
-#     return True
-
-# def find_nonzero_row(matrix, pivot_row, col):
-#     nrows = matrix.shape[0]
-#     for row in range(pivot_row, nrows):
-#         if matrix[row, col] != 0:
-#             return row
-#     return None
-
-# # Swapping rows so that we can have our non zero row on the top of the matrix
-# def swap_rows(matrix, row1, row2):
-#     matrix[[row1, row2]] = matrix[[row2, row1]]
-
-# def make_pivot_one(matrix, pivot_row, col):
-#     pivot_element = matrix[pivot_row, col]
-#     matrix[pivot_row] /= pivot_element
-#     # print(pivot_element)
-
-# def eliminate_below(matrix, pivot_row, col):
-#     nrows = matrix.shape[0]
-#     pivot_element = matrix[pivot_row, col]
-#     for row in range(pivot_row + 1, nrows):
-#         factor = matrix[row, col]
-#         matrix[row] -= factor * matrix[pivot_row]
-
-# # Implementing above functions
-# def row_echelon_form(matrix):
-#     nrows = matrix.shape[0]
-#     ncols = matrix.shape[1]
-#     pivot_row = 0
-# # this will run for number of column times. If matrix has 3 columns this loop will run for 3 times
-#     for col in range(ncols):
-#         nonzero_row = find_nonzero_row(matrix, pivot_row, col)
-#         if nonzero_row is not None:
-#             swap_rows(matrix, pivot_row, nonzero_row)
-#             make_pivot_one(matrix, pivot_row, col)
-#             eliminate_below(matrix, pivot_row, col)
-#             pivot_row += 1
-#     return matrix
-
-
-# matrix = np.array([[8., 5., -2., 4., 28.],[4., 2.5, 20., 4., -4.],[8., 7., 1., 4., 17.],])
-# print("Matrix Before Converting:")
-# print(matrix)
-# print()
-# result = row_echelon_form(matrix)
-# print("After Converting to Row Echelon Form:")
-# print(result)
-# if is_row_echelon_form(result):
-#     print("In REF")
-# else:
-#     print("Not in REF--------------->")
--- Exercise10/row_echelon_form.py
+++ Exercise10/row_echelon_form.py
@@ -5,46 +5,41 @@ K = TypeVar('K')
 
 
 def RowEchelonForm(cls: Type['Matrix[K]']) -> Type['Matrix[K]']:
+    
+ 
     def row_echelon(self) -> 'Matrix[K]':
         matrix = self.matrix
-        i = 0                # current pivot‐row
-        column = 0           # current column index
-
-        # Loop until we've either processed all rows or all columns
-        while i < self.row_count and column < self.column_count:
-            # 1) Find a nonzero entry in this column at or below row i
-            j = i
-            while j < self.row_count and matrix[j][column] == 0:
+        row_index, column_index, old_pivot_row = 0, 0, None
+        
+        
+        while row_index < self.row_count and column_index < self.column_count:
+            j = row_index
+            while j < self.row_count and matrix[j][column_index] == 0:
                 j += 1
-
-            # 2) If no pivot found in this column, move to next column
+            
             if j == self.row_count:
-                column += 1
+                column_index += 1
                 continue
 
-            # 3) Swap row j into row i
-            matrix = self.swap_rows(j, i, matrix)
+            matrix = self.swap_rows(j, row_index, matrix)
 
-            # 4) Scale row i so the pivot at (i, column) becomes 1
-            pivot_val = matrix[i][column]
-            matrix[i] = matrix[i].scl(1 / pivot_val)
+            pivot_val = matrix[row_index][column_index]
+            matrix[row_index].scl(1 / pivot_val)
 
-            # 5) Eliminate all entries below the pivot in this column
-            k = i + 1
-            while k < self.row_count:
-                factor = matrix[k][column]
-                if factor != 0:
-                    matrix[k] = matrix[k].add(matrix[i].scl(-factor))
-                k += 1
+            for row in range(self.row_count):
+                if row != row_index:
+                    factor = matrix[row][column_index] 
+                    if factor != 0:
+                        scaled_row = matrix[row_index]._copy()
+                        matrix[row] = matrix[row].sub(scaled_row.scl(factor))
 
-            # 6) Move to the next pivot‐row and next column
-            i += 1
-            column += 1
-
+            row_index += 1
+            column_index += 1
         return cls(matrix)
 
     if not hasattr(cls, "row_echelon"):
         setattr(cls, "row_echelon", row_echelon)
+
 
     return cls
 
@@ -53,152 +48,3 @@ def RowEchelonForm(cls: Type['Matrix[K]']) -> Type['Matrix[K]']:
 
 
 
-
-
-# def RowEchelonForm(cls: Type['Matrix[K]']) -> Type['Matrix[K]']:
-#     def row_echelon(self) -> 'Matrix[K]':
-#         matrix = self.matrix
-#         column = 0
-
-#         # Find the first pivot column and swap
-#         while column < self.column_count:
-#             i = 0
-#             while i < self.row_count and matrix[i][column] == 0:
-#                 i += 1
-#             if i < self.row_count:
-#                 matrix = self.swap_rows(i, 0, matrix)
-#                 matrix[0] = matrix[0].scl(1 / matrix[0][column])
-#                 break
-#             column += 1
-
-#         if column == self.column_count:
-#             return self.print(can_print=False, matrix=matrix)
-
-#         print("***************** first row found **********************")
-#         self.print(can_print=True, matrix=matrix)
-#         print("*********************************************\n\n")
-    
-
-#         pivot = {0: column}
-#         i = 1
-#         print(f"pivot position{pivot}")
-#         loop = -1
-#         last_pivot = list(pivot.values())[-1]
-#         while i < self.row_count:
-#             next_row = i + 1
-#             while matrix[i]._is_zero() and next_row < self.row_count:
-#                 matrix = self.swap_rows(i, next_row, matrix)
-#                 next_row += 1
-#             if matrix[i]._is_zero():
-#                 break
-
-#             column, pivot_position = 0, 0
-#             last_pivot = list(pivot.values())[-1]
-            
-#             # print(f"last pivot: {last_pivot}")
-#             while column <= last_pivot and column < self.column_count and pivot_position < len(pivot):
-#                 pivot_row = pivot[pivot_position]
-#                 print(f"matrix[{i}] = {matrix[i]}]")
-#                 if matrix[i][column] != 0 and matrix[pivot_row][column] != 0:
-#                     factor = -matrix[i][column] / matrix[pivot_row][column]
-#                     print(f"factor: {factor}")  
-#                     matrix[i] = matrix[i].add(matrix[pivot_row].scl(factor))
-#                 column += 1
-#                 pivot_position += 1
-
-#             # print(f"matrix[{i}] = {matrix[i]}]")
-            
-#             while column < self.column_count and matrix[i][column] == 0:
-#                 # print(f"matrix[{i}][{column}] = {matrix[i][column]}")
-#                 column += 1
-            
-#             pivot[i] = column
-#             i += 1
-#             # if column < self.column_count and matrix[i][column] == 0 and i < self.row_count - 1 and loop != i:
-#             #     loop = i
-#             #     matrix = self.swap_rows(i, i + 1, matrix)
-#             #     print(f"matrix after swap: {matrix[i]}")
-#             #     # break
-#             # else:
-#             #     print("\n\n")
-#             #     print(f"matrix[{i}]= {matrix[i]}")
-#             #     print(f"matrix[{i}][{column}] = {matrix[i][column]}")
-#             #     print("loop: ", loop)
-                
-#             #     # while column < self.column_count and matrix[i][column] == 0:
-#             #     #     column += 1
-#             #     loop = 0
-#             #     pivot[i] = column
-#             #     print(pivot)
-#         print("\n\n")
-#         return self.print(can_print=False, matrix=matrix)
-
-#     if not hasattr(cls, "row_echelon"):
-#         setattr(cls, "row_echelon", row_echelon)
-
-#     return cls
-
-
-
-
-
-
-# def RowEchelonForm(cls: Type['Matrix[K]']) -> Type['Matrix[K]']:
-#     def row_echelon(self) -> 'Matrix[K]':
-#         matrix = self.matrix
-
-#         print("\n\n")
-        
-#         column = 0
-#         while column < self.column_count:
-#             i = 0
-#             while i < self.row_count and matrix[i][column] == 0:
-#                 i += 1
-#             if i < self.row_count:
-#                 matrix = self.swap_rows(i, 0, matrix)
-#                 break
-#             column += 1
-#         if column == self.column_count:
-#             return self.print(can_print=False, matrix=matrix)
-#         pivot = {0: column}
-#         i = 1
-#         while i < self.row_count:
-#             next_row = i + 1
-#             while matrix[i]._is_zero() and next_row < self.row_count: 
-#                 matrix = self.swap_rows(i, next_row, matrix)
-#                 next_row += 1
-#             if matrix[i]._is_zero():
-#                 break
-
-#             column, pivot_position = 0, 0
-#             last_pivot =  list(pivot.values())[-1]
-#             while column <= last_pivot and column < self.column_count:
-#                 if matrix[i][column] != 0:
-#                     x = -matrix[i][column]/ matrix[pivot[pivot_position]][column]
-#                     matrix[i] =  matrix[i].add(matrix[pivot_position].scl(x))
-#                 column += 1
-#                 pivot_position += 1
-
-#             if  column < self.column_count and matrix[i][column] == 0 and i < self.row_count - 1:
-#                 print("since i am here there is a problem")
-#                 matrix = self.swap_rows(i, i + 1, matrix)
-#             else:
-#                 pivot[i] = column
-#                 i += 1
-#         print("\n\n")
-#         return self.print(can_print=False, matrix=matrix)
-#     if not hasattr(cls, "row_echelon"):
-#         setattr(cls, "row_echelon", row_echelon)
-    
-#     return cls
-
-
-        
-        
-        
-        # print("***************** first row found **********************")
-        # self.print(can_print=True, matrix=matrix)
-        # print("*********************************************\n\n")
-    
-    
-    
